a9388499449063d645e91931e228a04b2387bf7c
ruuuh20 <pkim2320@gmail.com>
2018-05-18 02:03:33 -0400

    initialize and set up

---
+++ src/components/Weather.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Weather = (props) => {
+    return (
+      <div>
+      { props.city && props.country && <p>Location: {props.city}, {props.country}</p> }
+      { props.temperature && <p>Temperature: {props.temperature}</p> }
+      { props.humidity && <p>humidity: {props.humidity}</p>}
+      { props.description && <p>Condition: {props.description}</p> }
+      { props.error && <p> {props.error}</p> }
+      </div>
+    )
+
+}
+
+export default Weather;
